a24b21113b2ebe2ae6a461622f7cc135632b5bfa
Artem Ryaguzov <artemkaryaguzov@gmail.com>
2018-11-01 15:32:03 +0400

    Initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,54 @@
+// Navigation scripts
+
+// Smooth Scrolling
+$('.navbar__link').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
+
+$('.mobile-nav__link').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
+
+
+// Side menu
+
+$('.navbar__sidemenu').click(function(e){
+    e.preventDefault();
+    $('.mobile-nav').css('width', '250px');
+});
+
+
+$('.mobile-nav__closebtn').click(function(e){
+    e.preventDefault();
+    $('.mobile-nav').css('width', '0');
+});
+
+// End of Navigation scripts
