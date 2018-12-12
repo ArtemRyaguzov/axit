1e1eee9c625f04984807eb3ab483e66916539ebd
Artem Ryaguzov <artemkaryaguzov@gmail.com>
2018-12-12 19:52:24 +0400

    fix after review and bundling and fullscreen

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
 
 // gulp.task = Define tasks
 // gulp.src = Point to files to use
@@ -19,20 +21,59 @@ gulp.task('message', function(){
 // });
 
 
+// Copy HTML
+// USAGE: gulp copy
+
+gulp.task('copy', function(){
+    gulp.src('src/html/*.html')
+    .pipe(gulp.dest('dist'));
+});
+
+
+// Optimize Images and move them to the dist
+// USAGE: gulp imageMin
+
+gulp.task('imageMin', () =>
+    gulp.src('src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/images'))
+);
+
+
+//  Concatenate js files
+// USAGE: gulp concat
+
+gulp.task('concat', function(){
+    gulp.src(['src/js/wow.js', 'src/js/main.js'])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/js'))
+});
+
+
 // Compile SASS
 // USAGE: gulp sass
 
 gulp.task('sass', function(){
-  gulp.src('scss/*.scss')
+  gulp.src('src/scss/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('dist/css'));
     // .pipe(gulp.watch('scss/*.scss'));
 });
 
 
-// Watching changes in sass files
+// DO ALL FUNCTIONS
+// USAGE: gulp build
+
+gulp.task('build', ['copy', 'imageMin', 'sass', 'concat']);
+
+
+
+// Watching changes
 // USAGE: gulp watch
 
 gulp.task('watch', function(){
-  gulp.watch('scss/*.scss', ['sass']);
+  gulp.watch('src/scss/*.scss', ['sass']);
+  gulp.watch('src/js/*.js', ['concat']);
+  gulp.watch('src/html/*.html', ['copy']);
+  gulp.watch('src/images/*', ['imageMin']);
 });
